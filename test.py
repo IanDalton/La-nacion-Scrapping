9167227d812a5c33f6033acdbe6ed84a6ed6ef53
IanDalton <daltioan@gmail.com>
2023-05-16 23:21:20 -0300

    all comments

--- test.py
+++ test.py
@@ -3,61 +3,71 @@ from lxml import html
 import pandas as pd
 import asyncio
 import subprocess, json
-
-async def get_data(url):
-    response = requests.get(url)
-    tree = html.fromstring(response.content)
-    element = tree.xpath('//*[@id="content"]/div[7]/div/div/h1')[0]
-    text = element.text
-    return text
-
 import re
 
-def extract_nota_id(script_text):
+async def extract_nota_id(script_text):
     match = re.search(r'"nota_id":\s*"([^"]+)"', script_text)
     if match:
         return match.group(1)
     else:
         return None
 
-def extract_comments(comment_id,last_comment=None):
-    url = f'https://livecomments.viafoura.co/v4/livecomments/00000000-0000-4000-8000-5611d514abb3?limit=100&container_id={comment_id}&reply_limit=3&sorted_by=newest'
+async def extract_comments(comment_id,last_comment=None,context_uuid = None):
+    url = 'https://livecomments.viafoura.co/v4/livecomments/00000000-0000-4000-8000-5611d514abb3'
+    if context_uuid:
+        url += f'/{context_uuid}/comments'
+    url += f'?limit=100&container_id={comment_id}&reply_limit=100&sorted_by=newest'
     if last_comment:
         url += f'&starting_from={last_comment}'
+    print(url)
     response = requests.get(url)
     response = response.json()
     comments = dict()
-    for comment in response.get('contents'):
-        comments[comment.get("content_uuid")] = {
-            "comment": comment.get("content"),
-            'time': comment.get("date_created"),
-            "total_likes": comment.get("total_likes"),
-            "total_dislikes": comment.get("total_dislikes"),
-            "total_replies": comment.get("total_replies"),
-            "is_actor_ghostbanned": comment.get("is_actor_ghostbanned"),
-        }
-        if comment.get("is_actor_ghostbanned"):
-            print(comment.get("content_uuid"))
-    if len(comments) == 100:
-        comments.update(extract_comments(comment_id,comment.get("content_uuid")))
+    uuid = None
+    try:
+        for comment in response.get('contents'):
+            if not uuid or uuid == last_comment or uuid != comment.get('content_container_uuid') :
+                uuid = comment.get("content_uuid")
+            comments[comment.get("content_uuid")] = {
+                "comment": comment.get("content"),
+                'time': comment.get("date_created"),
+                "total_likes": comment.get("total_likes"),
+                "total_dislikes": comment.get("total_dislikes"),
+                "total_replies": comment.get("total_replies"),
+                "is_actor_ghostbanned": comment.get("is_actor_ghostbanned"),
+            }
+            #print(comment.get("content_uuid"),comment.get("actor_uuid"))
+            if comment.get("is_actor_ghostbanned"):
+                print(comment.get("content_uuid"))
+        if len(comments) >= 100:
+            new_comments = await extract_comments(comment_id,uuid,comment.get('content_container_uuid'))
+            comments.update(new_comments)
+    except TypeError:
+        comments = response
     return comments
 
-
-url = "https://www.lanacion.com.ar/politica/el-presidente-se-reunio-con-empresarios-de-china-y-genero-inquietud-en-estados-unidos-nid16052023/"
-
-response = requests.get(url)
-tree = html.fromstring(response.content)
-
-# Use the provided XPath expression to find the desired table element
-table_element = tree.xpath("/html/head/script[4]")[0]
-
-# Convert the table element to a string and read it into a DataFrame
-table_html = html.tostring(table_element)
-
-# Display the resulting DataFrame
-with open('arch.json','w') as f:
-    comments = extract_comments(extract_nota_id(table_html.decode("utf-8")))
+async def get_info(url:str):
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+    table_element = tree.xpath("/html/head/script[4]")[0]
+    table_html = html.tostring(table_element).decode("utf-8")
+    title = url.split('/')
+    title = title[len(title)-2]
+    print(title)
+    comments = await extract_comments(await extract_nota_id(table_html))
     print(len(comments))
-    json.dump(comments,f)
+    
+    with open(f"{title}.json","w") as f:
+        json.dump(comments,f)
 
 
+async def main():
+    urls = ["https://www.lanacion.com.ar/politica/la-carta-completa-de-cristina-kirchner-en-la-que-volvio-a-rechazar-ser-candidata-no-voy-a-entrar-en-nid16052023/",
+            "https://www.lanacion.com.ar/politica/cristina-kirchner-no-sera-candidata-las-repercusiones-a-la-salida-del-congreso-del-pj-nid16052023/",
+            "https://www.lanacion.com.ar/politica/cristina-kirchner-ratifico-que-no-sera-candidata-no-voy-a-ser-mascota-del-poder-nid16052023/"]
+
+    async with asyncio.TaskGroup() as tg:
+        for url in urls:
+            tg.create_task(get_info(url))
+
+asyncio.run(main())
